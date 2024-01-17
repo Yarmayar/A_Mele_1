45120d94d14da14afc77b1fb291537a792b4c9cc
Yarmayar <my4and@gmail.com>
2024-01-18 01:10:29 +0300

    Меняем представления на CBV (классы) - все они наследуют от класса View, который служит для диспетчеризации HTTP-методов и других распространенных функциональностей. ListView передает запрошенную страницу в переменную с именем page_obj.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,20 +1,30 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3, orphans=2)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3, orphans=2)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
+    paginate_by = 3
+    paginate_orphans = 2
 
 
 def post_detail(request, post, year, month, day):
