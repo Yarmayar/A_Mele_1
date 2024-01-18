15600bde5ae58a2f595e3e942c0171c16ae72c10
Yarmayar <my4and@gmail.com>
2024-01-18 21:48:44 +0300

    Создание системы комментариев: 1. Create model Comment 2. Add model to admin panel 3. Create form for Comment 4. Create view for Comment -используем предоставляемый веб-фреймворком Django декоратор require_POST, чтобы разрешить только запросы методом POST 5. Create URL for view post_comment 6. Create the comment_form.html in new directory 'templates/blog/post/includes' 7. Create the comment.html in directory 'templates/blog/post' (Шаблон представления комментариев к посту) 8. Refactor post_detail view for adding comments related to the post 9. Refactor detail.html for display comments count, list of comments and form for new comment.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
@@ -36,7 +37,10 @@ def post_detail(request, post, year, month, day):
                              publish__month=month,
                              publish__day=day,
                              status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -47,7 +51,9 @@ def post_share(request, post_id):
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            # print(post.get_absolute_url())  - /blog/2024/1/16/benjamin-franklin-s-inventions/
             post_url = request.build_absolute_uri(post.get_absolute_url())
+            # request.build_absolute_uri(post.get_absolute_url())  - http://127.0.0.1:8000/blog/2024/1/16/benjamin-franklin-s-inventions/
             subject = f'{cd["name"]} recommends you read {post.title}'
             message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
             send_mail(subject, message, 'alykovyaroslav@gmail.com', [cd['to']])
@@ -56,4 +62,17 @@ def post_share(request, post_id):
         form = EmailPostForm()
 
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
 
