bd53bb41278af295c0665aafaffe344abf472a08
Yarmayar <my4and@gmail.com>
2024-01-18 17:58:06 +0300

    Рекомендация постов по электронной почте: 1. Create form EmailPostForm 2. Create view post_share (Список ошибок валидации можно получить посредством form.errors Если данные формы не проходят валидацию, то cleaned_data будет содержать только валидные поля) 3. Create share.html 4. Create url -------------------- Отправка электронных писем с помощью Django: 1. Указываем настроечные параметры, которые позволяют определять конфигурацию SMTP, чтобы отправлять электронные письма с помощью Django 2. Test through shell: from django.core.mail import send_mail send_mail('Django mail', 'This e-mail was send with Django.', 'alykovyaroslav@gmail.com', ['alykovyaroslav@gmail.com', 'my4and@gmail.com'], fail_silently=False) 3. Use django-mail functionality in view post_share Поскольку ссылка на пост должна вставляться в электронное письмо, мы получаем абсолютный путь к посту, используя его метод get_absolute_url(). Мы используем этот путь на входе в метод request.build_absolute_uri(), чтобы сформировать полный URL-адрес, включая HTTP-схему и хост-имя (hostname)1. 4. Change detail.html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
 
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -35,3 +37,23 @@ def post_detail(request, post, year, month, day):
                              publish__day=day,
                              status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.POST:
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cd["name"]} recommends you read {post.title}'
+            message = f"Read {post.title} at {post_url}\n\n{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'alykovyaroslav@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
