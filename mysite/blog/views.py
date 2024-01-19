3a15fb83ec9faed516db7e49606af7a2474afff9
Yarmayar <my4and@gmail.com>
2024-01-19 16:33:53 +0300

    Добавление функциональности тегирования, django-taggit – это приспособленное для реиспользования приложение, которое в первую очередь предлагает модель Tag и менеджер для удобного добавления тегов в любую модель. 1. pip install django-taggit==3.0.0 - добавляем 'taggit' в INSTALLED_APPS - добавить предоставляемый приложением django-taggit менеджер TaggableManager в модель Post - сделать миграцию 2. refactor list.html 3. refactor post_list (view) 4. create new URL for post_list by tag

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,31 +3,37 @@ from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3, orphans=2)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    paginator = Paginator(post_list, 3, orphans=2)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    template_name = 'blog/post/list.html'
-    paginate_by = 3
-    paginate_orphans = 2
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     template_name = 'blog/post/list.html'
+#     paginate_by = 3
+#     paginate_orphans = 2
 
 
 def post_detail(request, post, year, month, day):
